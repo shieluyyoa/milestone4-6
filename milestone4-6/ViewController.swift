dd8d12bba49cac5d0a62b8f33e4d9cd4c6fd8280
Oscarlui <oscarlu1@connect.hku.hk>
2022-02-25 03:28:07 +0800

    Initial Commit

---
+++ milestone4-6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  milestone4-6
+//
+//  Created by Oscar Lui on 25/2/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
